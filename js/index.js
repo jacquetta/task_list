8a892cf3c40a9f8a81031150162c04d4397436a7
jacque_viola <jacqueviolaux@gmail.com>
2021-05-12 15:19:53 -0500

    added validation to JavaScript

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const newTaskNameInput = document.getElementById('newTaskNameInput');
+const newTaskDescriptionInput = document.getElementById('newTaskDescriptionInput');
+const newTaskAssignedInput = document.getElementById('newTaskAssignedInput');
+const newTaskDueDateInput = document.getElementById('newTaskDueDateInput');
+const newStatusTask = document.getElementById('newStatusTask');
+
+function validFormFieldInput(data){
+    const nameTask = data.value;
+    console.log("name: " + nameTask);
+}
+
+// validFormFieldInput(newTaskNameInput);
+// validFormFieldInput(newTaskDescriptionInput);
+// validFormFieldInput(newTaskAssignedInput);
+// validFormFieldInput(newTaskDueDateInput);
+// validFormFieldInput(newStatusTask);
+
+
+  function validateForm() {
+    let y = document.forms["formName"]["taskName"].value;
+    if (y == "") {
+      alert("Fill out all inputs!");
+      let alertDialog = document.getElementById('submitBtn');
+      alertDialog.style.display = 'block';
+      return false;
+    }
+  }
+
